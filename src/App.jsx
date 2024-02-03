df4aaf81702d1b90f44cd5d57e5b0ebb03148ac4
Emediong Bassey Edem <edememediong1@gmail.com>
2024-02-03 11:28:53 +0100

    Working with Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Navbar from "./Components/navbar"
 import market from "./data/json"
 import Product from "./Components/products"
@@ -5,11 +6,27 @@ import  "./index.css";
 import "./App.css";
 
 
-function App() {
-  const products = market.map((item) => {
-    return <Product {...item} />
-  })
+function MyButton({onSelect, count}){
 
+  return(
+    <div>
+        <button  onClick={onSelect} >The Count is {count}</button>
+    </div>
+
+  )
+}
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  function handleClick(){
+    setCount(count + 1)
+  }
+
+  const products = market.map((item) => {
+    return <Product key={item.product_id} {...item} />
+  })
   return (
     <section>
       <header>
@@ -17,6 +34,8 @@ function App() {
       </header>
       <main className="product-display">
       {products}
+      <MyButton onSelect={handleClick} count={count}/>
+      <MyButton onSelect={handleClick} count={count}/>
       </main>
     </section>
   );
